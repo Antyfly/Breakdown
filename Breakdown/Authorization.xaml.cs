2189f07c05625a4d8b5bd1aef4fa92cf31731744
antyfly <julia290102@mail.ru>
2021-04-20 21:31:32 +0300

    Добавьте файлы проекта.

---
+++ Breakdown/Authorization.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Breakdown.Entity;
+
+namespace Breakdown
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+            var datasourse = CN.content.Client.ToList();
+            datasourse = datasourse.Take(50).ToList() ;
+            List.ItemsSource = datasourse;
+
+        }
+
+    }
+}
---
+++ Breakdown/Entity/CN.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakdown.Entity
+{
+    public static class CN
+    {
+        public static Entities content = new Entities();
+    }
+}
